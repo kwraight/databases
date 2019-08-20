ebc487705b8794efe2fb5675baf5d2b0f1bcbb33
kwraight <kenneth.gibb.wraight@gmail.com>
2019-08-20 16:24:03 +0100

    Create mongoDB.py

---
+++ mongoDB.py
@@ -0,0 +1,11 @@
+
+
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient("mongodb+srv://kwraight:<password>@cluster0-yrb5p.mongodb.net/test?retryWrites=true&w=majority")
+db=client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
